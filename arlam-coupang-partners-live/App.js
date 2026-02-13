61e586ec9f4715c0469b8d18a6aee7d6f7c965ab
yeonu0007-arch <yeonu0007@gmail.com>
2026-02-13 10:13:46 +0900

    Renamed App.tsx to App.js and updated import paths.

---
+++ arlam-coupang-partners-live/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './types.ts';
+import './services/geminiService.ts';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+};
+
+export default App;
